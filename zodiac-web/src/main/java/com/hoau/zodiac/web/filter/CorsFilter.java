86d750acfda8ed2c2e93adf205f0c1b13d1552fa
陈宇霖 <yulin.chen@itiaoling.com>
2017-08-22 14:25:28 +0800

    陈宇霖:增加PUT类型请求允许跨域

--- zodiac-web/src/main/java/com/hoau/zodiac/web/filter/CorsFilter.java
+++ zodiac-web/src/main/java/com/hoau/zodiac/web/filter/CorsFilter.java
@@ -24,7 +24,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest reqs = (HttpServletRequest) req;
         response.setHeader("Access-Control-Allow-Origin", reqs.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "access-control-allow-headers,access-control-allow-origin,content-type");
 
