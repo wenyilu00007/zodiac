e92668e64e9a7edd128d609d8dcf8dd16aa6b569
dingyong <dingyong@hoau.net>
2017-08-28 16:01:31 +0800

    commit by DingYong 回滚

--- zodiac-web/src/main/java/com/hoau/zodiac/web/request/BaseVO.java
+++ zodiac-web/src/main/java/com/hoau/zodiac/web/request/BaseVO.java
@@ -8,63 +8,11 @@ package com.hoau.zodiac.web.request;
  * @description vo 基类
  * @date 2017/8/6
  */
-public class BaseVO<T> {
-
-    /**
-     * 通用查询参数
-     */
-    private String query;
-
-    /**
-     * 实际值
-     */
-    private String value;
-
-    /**
-     * 显示值
-     */
-    private String displayName;
-
-    /**
-     * 返回的实体对象
-     */
-    private T result;
+public class BaseVO {
 
     private int page;
 
     private int limit;
-
-    public String getQuery() {
-        return query;
-    }
-
-    public void setQuery(String query) {
-        this.query = query;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public T getResult() {
-        return result;
-    }
-
-    public void setResult(T result) {
-        this.result = result;
-    }
 
     public int getPage() {
         return page;
