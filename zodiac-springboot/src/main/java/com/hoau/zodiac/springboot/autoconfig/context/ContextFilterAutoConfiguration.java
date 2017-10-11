0be55fa28725b66adb16f51aebda23137622fc31
陈宇霖 <yulin.chen@itiaoling.com>
2017-10-11 23:41:34 +0800

    陈宇霖:增加在使用网关时后台获取当前操作用户信息的过滤器

--- zodiac-springboot/src/main/java/com/hoau/zodiac/springboot/autoconfig/context/ContextFilterAutoConfiguration.java
+++ zodiac-springboot/src/main/java/com/hoau/zodiac/springboot/autoconfig/context/ContextFilterAutoConfiguration.java
@@ -1,7 +1,7 @@
 package com.hoau.zodiac.springboot.autoconfig.context;
 
 import com.hoau.zodiac.web.filter.ContextFilter;
-import com.hoau.zodiac.web.filter.CorsFilter;
+import com.hoau.zodiac.web.filter.GatewayContextFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -21,23 +21,40 @@ import java.util.Arrays;
 */
 @Configuration
 @EnableConfigurationProperties(ContextFilterProperties.class)
-@ConditionalOnProperty(prefix = "zodiac.web.context", name = "enable")
 public class ContextFilterAutoConfiguration {
 
     @Autowired
     private ContextFilterProperties contextFilterProperties;
 
     /**
-     * 创建框架使用的context拦截器，初始化RequestContext\SessionContext\UserContext
+     * 创建没有通过网关访问的系统使用的context过滤器，初始化RequestContext\SessionContext\UserContext
      * @return
      * @author 陈宇霖
      * @date 2017年08月07日21:35:59
      */
     @Bean
     @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "zodiac.web.context", name = "enable")
     public ContextFilter contextFilter() {
         ContextFilter contextFilter = new ContextFilter();
         contextFilter.setExcludeUrlPatterns(contextFilterProperties.getExcludeUrlPatterns());
         return contextFilter;
+    }
+
+    /**
+     * 创建使用网关访问系统的系统的context过滤器，初始化RequestContext\SessionContext\UserContext
+     * @return
+     * @author 陈宇霖
+     * @date 2017年10月11日18:48:35
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "zodiac.web.context", name = "gatewayEnable")
+    public GatewayContextFilter gatewayContextFilter() {
+        GatewayContextFilter gatewayContextFilter = new GatewayContextFilter();
+        gatewayContextFilter.setExcludeUrlPatterns(contextFilterProperties.getExcludeUrlPatterns());
+        gatewayContextFilter.setGatewayLogoutServerUrl(contextFilterProperties.getGatewayLogoutServerUrl());
+        gatewayContextFilter.setGatewayLogoutRedirectUrl(contextFilterProperties.getGatewayLogoutRedirectUrl());
+        return gatewayContextFilter;
     }
 }
--- zodiac-springboot/src/main/java/com/hoau/zodiac/springboot/autoconfig/context/ContextFilterProperties.java
+++ zodiac-springboot/src/main/java/com/hoau/zodiac/springboot/autoconfig/context/ContextFilterProperties.java
@@ -21,12 +21,51 @@ public class ContextFilterProperties {
     private boolean enable;
 
     /**
+     * 是否启用通过网关，gatewayEnable和enable不能同时为true，否则用户信息读取会有问题
+     */
+    private boolean gatewayEnable;
+
+    /**
+     * 启用网关情况下，服务器登陆地址
+     */
+    private String gatewayLogoutServerUrl;
+
+    /**
+     * 启用网关情况下，登出后重定向地址
+     */
+    private String gatewayLogoutRedirectUrl;
+
+    /**
      * 不进行过滤的地址列表
      */
     private List<String> excludeUrlPatterns;
 
     public boolean isEnable() {
         return enable;
+    }
+
+    public boolean isGatewayEnable() {
+        return gatewayEnable;
+    }
+
+    public void setGatewayEnable(boolean gatewayEnable) {
+        this.gatewayEnable = gatewayEnable;
+    }
+
+    public String getGatewayLogoutServerUrl() {
+        return gatewayLogoutServerUrl;
+    }
+
+    public void setGatewayLogoutServerUrl(String gatewayLogoutServerUrl) {
+        this.gatewayLogoutServerUrl = gatewayLogoutServerUrl;
+    }
+
+    public String getGatewayLogoutRedirectUrl() {
+        return gatewayLogoutRedirectUrl;
+    }
+
+    public void setGatewayLogoutRedirectUrl(String gatewayLogoutRedirectUrl) {
+        this.gatewayLogoutRedirectUrl = gatewayLogoutRedirectUrl;
     }
 
     public void setEnable(boolean enable) {
