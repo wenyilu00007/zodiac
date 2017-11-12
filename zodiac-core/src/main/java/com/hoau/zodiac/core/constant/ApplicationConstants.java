d82256228393e419afbeb9db8bd77dc5cd8b5a42
刘德云 <deyun.liu@itiaoling.com>
2017-11-12 18:05:13 +0800

    刘德云：修改常量类

--- zodiac-core/src/main/java/com/hoau/zodiac/core/constant/ApplicationConstants.java
+++ zodiac-core/src/main/java/com/hoau/zodiac/core/constant/ApplicationConstants.java
@@ -38,10 +38,68 @@ public class ApplicationConstants {
      * 停用
      */
     public static final int DISABLE = 0;
+    /**
+     * 带电
+     */
+    public static final int CHARGED_YES = 0;
+    /**
+     * 不带电
+     */
+    public static final int CHARGED_NO = 1;
+    /**
+     * DDU
+     */
+    public static final int INCOTERM_DDU = 0;
+    /**
+     * DDP
+     */
+    public static final int INCOTERM_DDP = 1;
+
 
     /**
      * 系统自动操作数据的操作人
      */
     public static final String SYSTEM_OPERATOR = "SYSTEM";
+
+    /**
+     * 重量单位 克
+     */
+    public static final String WEIGHT_UNIT_G = "0";
+
+    /**
+     * 重量单位 千克
+     */
+    public static final String WEIGHT_UNIT_KG = "1";
+    /**
+     * 长度单位 厘米
+     */
+    public static final String LENGTH_UNIT_CM = "0";
+    /**
+     * 长度单位 米
+     */
+    public static final String WEIGHT_UNIT_M = "1";
+    /**
+     * 长度单位 毫米
+     */
+    public static final String WEIGHT_UNIT_MM = "3";
+
+    /**
+     *  币种：美元
+     */
+    public static final String CURRENCY_USD = "USD";
+    /**
+     * 币种：人民币
+     */
+    public static final String CURRENCY_CNY = "CNY";
+    /**
+     * 币种：台湾币
+     */
+    public static final String CURRENCY_TWD = "TWD";
+    /**
+     * 币种：港币
+     */
+    public static final String CURRENCY_HKD = "HKD";
+
+
 
 }
